726847ef2394a1f4a702eda4d66f4853227caf65
hokuto.hiarno2 <hokuto.hiarno2@evidentscientific.com>
2023-07-02 17:37:55 +0900

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Clipboard} from '@angular/cdk/clipboard';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'clipboard-copy-sample';
+  constructor(private clipboard: Clipboard) {}
+
+  onClickTextCopy() {
+    this.clipboard.copy('https://sample1.com');
+  }
 }
